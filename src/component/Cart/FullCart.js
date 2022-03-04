e578bb862d12893fa468d16dcb1c8373a0ae74e6
Xavi Yamuza <xavi_ym@hotmail.com>
2022-03-04 17:07:49 +0100

    Add-product.qty

--- src/component/Cart/FullCart.js
+++ src/component/Cart/FullCart.js
@@ -1,12 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ProductContext from "./../../context/ProductContext";
 
 export default function FullCart() {
     // Carregar el context
     const { carreto, setCarreto } = useContext(ProductContext);
-    console.log(carreto)
+    
+    
     // Calculamos el importe total
-   
+
     const totalCart = carreto.reduce((total, product) =>{
         return total+=parseFloat(product.preu)*product.qty;
     },0);
@@ -27,15 +28,34 @@ export default function FullCart() {
                 <td><img src={"pccomp/" + product.imatge} style={{ width: 50 }} /></td>
                 <td>{product.marca} {product.model} {product.processador}/{product.ram}/{product.emmagatzematge}/</td>
               
-                <td>  <input  type="number" min="0" value={product.qty}/> </td> 
-                <td>{product.preu}</td> 
-                <td>{product.preu}</td>   
+                <td>  <input  type="number" 
+                              className="qty"
+                              min="0" 
+                              value={product.qty}
+                              onChange={(e) => {
+                              
+                                // 1r Cogemos la cantidad de ese producto y le asignamos el nuevo valor
+                              setCarreto([...carreto], product.qty =  e.target.value);
+                            
+                              // 2n Eliminamos del carrito los elementos con qty = 0
+                                //carreto.filter(product => product.qty(''))
+                                //console.log("algo")
+                                  }}
+                                
+                                
+                                
+                              /></td> 
+                <td>{product.preu}€</td> 
+                <td>{parseFloat(product.preu * product.qty)}€</td>   
              
             </tr>
            
             
         ))}
-        <p>Total {totalCart}</p>   
+        
+            <p className="subtotal">Subtotal  <b className="total">{totalCart}€</b> </p>     
+        
+        
 </table>
     );
             }
